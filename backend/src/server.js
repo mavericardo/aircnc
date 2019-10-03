07ad762edcd73a3e220893b2963ba3ce86b8f5c5
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-03 01:50:17 -0300

    Cors and adjust connection with database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 
 //connect with database
-mongoose.connect("mongodb://localhost:27017/aircnc", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://marcos:marcos@aircnc-y7maq.mongodb.net/marcos?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
