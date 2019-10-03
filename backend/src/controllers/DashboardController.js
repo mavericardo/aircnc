7902db0ce5ccaa9efe73777581f38ff09b258b04
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-03 02:18:37 -0300

    Adjust in DashboardController - show

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -1,11 +1,11 @@
 const Spot = require("../models/Spot");
 
 module.exports = {
-  show(request, response) {
+  async show(request, response) {
     const { user_id } = request.headers;
 
-    const spots = Spot.find({ user: user_id });
-
+    const spots = await Spot.find({ user: user_id });
+    console.log(spots);
     return response.json(spots);
   }
 };
