cebba71abdd31e81d0ca8bbd62c518add7137bc9
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-05 23:29:36 -0300

    Finish dashboard
    - Stylize
    - Add Link to /New

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard()  {
     const [spots, setSpots] = useState([]);
@@ -8,6 +10,7 @@ export default function Dashboard()  {
         async function loadSpots(){
             const user_id = localStorage.getItem("user");
             const response = await api.get("/dashboard", { headers: { user_id } } );
+            console.log(response.data);
             setSpots(response.data);
         }
 
@@ -20,13 +23,19 @@ export default function Dashboard()  {
                 {
                     spots.map(spot => (
                         <li key={spot._id}>
-                            <header/>
+                            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
                             <strong>{spot.company}</strong>
-                            <span>{spot.price}</span>
+                            <span>{spot.price ? `R$${spot.price}/dia` : 'GRATUITO'}</span>
                         </li>
                     ))
                 }
             </ul>
+
+            <Link to="/new">
+               <button className="btn">
+                 Cadastrar novo spot
+               </button> 
+            </Link>
         </>
     );
 }
