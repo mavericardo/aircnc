12239bf7ec6fbc0f560f21332e49eafa10e1b9a8
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-05 23:30:30 -0300

    Link to image Spot
    - virtual field "thumbnail_url"

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 
-const SpotSchema = new mongoose.Schema({
-  thumbnail: String,
-  company: String,
-  price: String,
-  techs: [String],
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+const SpotSchema = new mongoose.Schema(
+  {
+    thumbnail: String,
+    company: String,
+    price: String,
+    techs: [String],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
   }
+);
+
+SpotSchema.virtual("thumbnail_url").get(function() {
+  return `http://localhost:8080/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model("Spot", SpotSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const path = require("path");
 const app = express();
 
 //connect with database
@@ -15,6 +16,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(8080);
