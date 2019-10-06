1f5abb4e11674e6c91db3bf49530ef0785586668
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-06 08:30:57 -0300

    Mobile - Routes

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Login() {
-  return <View />;
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
 }
