58e165d1548d81681a9cc326fb83b6a58d982c05
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-06 12:30:10 -0300

    Mobile - List spot

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,45 @@
-import React from "react";
-import { View } from "react-native";
-
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  AsyncStorage,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView
+} from "react-native";
+import logo from "../assets/logo.png";
+import SpotList from "../components/SpotList";
 export default function Login() {
-  return <View />;
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storagedTechs => {
+      const techsArray = storagedTechs.split(",").map(tech => tech.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo}></Image>
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10
+  }
+});
