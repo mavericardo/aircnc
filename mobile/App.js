97e01dd98444b0c3493bdb3d137d05389bbfa3da
Marcos Ricardo <mricardoalves99@gmail.com>
2019-10-06 23:37:34 -0300

    Mobile - List
    Receive response of reserva do spot in realtime.

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Routes from "./src/routes";
+
+import { YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
+
 export default function App() {
   return <Routes />;
 }
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -5,12 +5,30 @@ import {
   Text,
   Image,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  Alert
 } from "react-native";
 import logo from "../assets/logo.png";
+import socketio from "socket.io-client";
 import SpotList from "../components/SpotList";
 export default function Login() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.15.10:3333", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storagedTechs => {
